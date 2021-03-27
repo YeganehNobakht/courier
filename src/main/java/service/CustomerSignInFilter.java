65e3cd8c2987651ffc831d2824462b534c819111
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-03-26 21:28:17 -0700

    Add filter to check customer information from database.

---
+++ src/main/java/service/CustomerSignInFilter.java
@@ -0,0 +1,44 @@
+package service;
+
+import data.EntityDao;
+import model.Customer;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebFilter(filterName = "CustomerSignInFilter")
+public class CustomerSignInFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+
+        try {
+            String username = req.getParameter("username");
+            String password = req.getParameter("password");
+            Customer customer = EntityDao.fetchCustomerByUsername(username);
+
+            if (customer != null && customer.getPassword().equals(password)) {
+                chain.doFilter(req, resp);
+            }
+            else {
+                writer.println("Invalid username or password");
+                req.getRequestDispatcher("customerSignIn.html").include(req, resp);
+            }
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
