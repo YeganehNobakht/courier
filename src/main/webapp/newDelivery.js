d77ee8e454329bed849683bf339e1eea36f3ce58
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-03-29 02:43:45 -0700

    Add java script file for newDelivery page.

---
+++ src/main/webapp/newDelivery.js
@@ -0,0 +1,56 @@
+window.document.getElementById("noOptions").style.display = "block";
+window.document.getElementById("allOptions").style.display = "none";
+function changeOptions() {
+    var form = window.document.getElementById("form");
+    var docOption = window.document.getElementById("docOption");
+    var nDocOption = window.document.getElementById("nDocOption");
+
+    window.document.getElementById("noOptions").style.display = "none";
+
+    if (form.radioButton1.checked) {
+        nDocOption.style.display = "none";
+        docOption.style.display = "non";
+        docOption.selectedIndex = 0;
+    } else if (form.radioButton2.checked) {
+        docOption.style.display = "none";
+        nDocOption.style.display = "block";
+        nDocOption.selectedIndex = 0;
+    }
+
+}
+window.document.getElementById("radioButton1").onclick = changeOptions;
+window.document.getElementById("radioButton2").onclick = changeOptions;
+
+
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+
+
+function premium(){
+    var weight =
+        document.forms["form"]["weight"];
+    console.log("function premium")
+    var pricePerKg = 20000;
+    var x3 = weight.value *pricePerKg;
+    document.getElementById("premiumPriceResult").innerHTML = "price: "+x3;
+
+}
+function silver(){
+    console.log("function s")
+    var weight =
+        document.forms["form"]["weight"];
+    var pricePerKg = 10000;
+    var x3 = weight.value*pricePerKg;
+    document.getElementById("silverPriceResult").innerHTML = "price: "+x3;
+}
+function gold(){
+    console.log("function g")
+    var weight =
+        document.forms["form"]["weight"];
+    var pricePerKg = 14000;
+    var x3 = weight.value*pricePerKg;
+    document.getElementById("goldPriceResult").innerHTML = "price: "+x3;
+}
