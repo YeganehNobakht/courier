71f3be67d8aaef098f3055f863dfdc7e94a2894a
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-03-30 12:23:37 -0700

    Creating TrackCodeServlet.

---
+++ src/main/java/service/TrackCodeServlet.java
@@ -0,0 +1,53 @@
+package service;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "TrackCodeServlet")
+public class TrackCodeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        HttpSession session = request.getSession(false);
+        if (session.getAttribute("name") != null) {
+            writer.println("Welcome " + session.getAttribute("name"));
+            writer.println("<!DOCTYPE html>\n" +
+                    "<html lang=\"en\">\n" +
+                    "<link rel=\"stylesheet\" href=\"firstpagestyle.css\">\n" +
+                    "<head>\n" +
+                    "    <meta charset=\"UTF-8\">\n" +
+                    "    <title>trackCode</title>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "<div class=\"content\">\n" +
+                    "    <form  method=\"post\">\n" +
+                    "\n" +
+                    "        <center >\n" +
+                    "            <img src=\"trackCode1.png\" style=\"width: 200px;height: 200px;\" alt=\"\">\n" +
+                    "        </center>\n" +
+                    "            <center>\n" +
+                    "            <input  type=\"text\" name=\"customerTrack\" placeholder=\"Enter Your Track Code\">\n" +
+                    "            <button type=\"submit\">Tracking</button>\n" +
+                    "            </center>\n" +
+                    "            <br><br>\n" +
+                    "    </form>\n" +
+                    "</div>\n" +
+                    "</body>\n" +
+                    "</html>");
+        }else{
+            writer.println("Please login first");
+            request.getRequestDispatcher("customer.html").include(request, response);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
