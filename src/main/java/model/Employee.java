87dd54ab2ae777fac2d5147a2c61b022bf5a2330
masoune nobakht <m.yeganeh.n1994@gmail.com>
2021-04-06 20:16:54 -0700

    Adding Employee class.

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,4 +1,91 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
+    @Id
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private String name;
+    @Column
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String phone;
+    @Column
+    private String address;
+
+
+    public Employee(String username, String password, String name, String lastName, String email, String phone, String address) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+    public Employee(){}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
